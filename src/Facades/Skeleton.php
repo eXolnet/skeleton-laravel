c7ccabc3993880072556d23514372ea09ab3f331
Alexandre D'Eschambeault <adeschambeault@exolnet.com>
2021-09-22 15:34:34 -0400

    Move skeleton facade to the Facades namespace

--- src/Facades/Skeleton.php
+++ src/Facades/Skeleton.php
@@ -1,20 +1,20 @@
 <?php
 
-namespace Exolnet\Skeleton;
+namespace Exolnet\Skeleton\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
 /**
  * @see \Exolnet\Skeleton\SkeletonClass
  */
-class SkeletonFacade extends Facade
+class Skeleton extends Facade
 {
     /**
      * Get the registered name of the component.
      *
      * @return string
      */
-    protected static function getFacadeAccessor()
+    protected static function getFacadeAccessor(): string
     {
         return 'skeleton';
     }
